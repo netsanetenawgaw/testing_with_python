520dda1eb72b77577ffb6f97623d955671fd5478
netsanetenawgaw <netsanetenawgaw5@gmail.com>
2025-11-06 10:55:19 +0300

    Correct user input handling and output formatting
    Fix variable names and print function errors.

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,10 @@
-# your code here
+# Get user information
+namme = input ("what's your name? ")
+favofite _food =input("what's your your favorite food? ")
+current_year = int(input("what's the current year? "))
+age =int(input("How old are you? "))
+
+# calculet birth year
+birth _year = current _year - age
+prinet(f"\nHello{name}! It's great to meet someone who loves {favorite_food}.") 
+prinet ("Based on the information your provided, you were bore in {birth_year}.")
